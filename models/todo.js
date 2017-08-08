9969223128cfc713c0ef6ff350afd144433b2000
Tristan Schmitt <trschmitt50@live.com>
2017-08-08 16:44:36 -0400

    edited html and began editing seed file

--- models/todo.js
+++ models/todo.js
@@ -2,24 +2,22 @@
 
 const sequelize = require('sequelize');
 
-const item = sequelize.define('item', {
+const todo = sequelize.define('todo', {
   title: { type: Sequelize.STRING },
-  description: { type: Sequelize.STRING },
   complete: { type: Sequelize.BOOLEAN }
 });
 
 // force: true will drop the table if it already exists
-item.sync({force: true}).then(() => {
+todo.sync({force: true}).then(() => {
   // Table created
-  return item.create({
+  return todo.create({
     title: 'take a nap',
-    description: 'get some sleep bruh',
     complete: false
   });
 });
 
-item.findAll().then(users => {
-  console.log(users)
+todo.findAll().then(item => {
+  console.log(item)
 })
 
 module.exports = function(sequelize, DataTypes) {
